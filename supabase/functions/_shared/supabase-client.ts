3af204cd7deaba030e934428d4c33e94a2907ac1
stem734 <stemurdock@gmail.com>
2026-04-16 20:35:11 +0100

    edge fix again

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -7,7 +7,9 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 export function createServiceClient() {
   return createClient(
     Deno.env.get('SUPABASE_URL')!,
-    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!,
+    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ||
+      Deno.env.get('SUPABASE_SECRET_KEY') ||
+      Deno.env.get('SB_SECRET_KEY')!,
   );
 }
 
@@ -18,7 +20,9 @@ export function createServiceClient() {
 export function createUserClient(authHeader: string) {
   return createClient(
     Deno.env.get('SUPABASE_URL')!,
-    Deno.env.get('SUPABASE_ANON_KEY')!,
+    Deno.env.get('SB_PUBLISHABLE_KEY') ||
+      Deno.env.get('SUPABASE_PUBLISHABLE_KEY') ||
+      Deno.env.get('SUPABASE_ANON_KEY')!,
     { global: { headers: { Authorization: authHeader } } },
   );
 }
@@ -35,7 +39,9 @@ export async function getAuthUser(authHeader: string | null) {
   const token = authHeader.replace('Bearer ', '');
   const supabase = createClient(
     Deno.env.get('SUPABASE_URL')!,
-    Deno.env.get('SUPABASE_ANON_KEY')!,
+    Deno.env.get('SB_PUBLISHABLE_KEY') ||
+      Deno.env.get('SUPABASE_PUBLISHABLE_KEY') ||
+      Deno.env.get('SUPABASE_ANON_KEY')!,
   );
 
   const { data, error } = await supabase.auth.getClaims(token);
