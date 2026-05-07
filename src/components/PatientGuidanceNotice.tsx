8ac3bfd65060f34144cd0c3869e8fa832356103c
stem734 <stemurdock@gmail.com>
2026-05-07 08:36:02 +0100

    nhs guidelines update

--- src/components/PatientGuidanceNotice.tsx
+++ src/components/PatientGuidanceNotice.tsx
@@ -7,7 +7,6 @@ type PatientGuidanceNoticeProps = {
 const PatientGuidanceNotice: React.FC<PatientGuidanceNoticeProps> = ({ text }) => {
   return (
     <div className="data-indicator data-indicator--nhs no-print" role="note" aria-label="Guidance notice">
-      <img className="data-indicator__logo" src="/nhs-wordmark-blue.jpg" alt="NHS" />
       <span>{text}</span>
     </div>
   );
